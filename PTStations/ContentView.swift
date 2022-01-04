ca2717a066daee4c9e2df23bd9ed55e3a845298a
Joao Pires <mail@joaopires.dev>
2022-01-04 08:24:21 +0000

    Initial Commit

---
+++ PTStations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PTStations
+//
+//  Created by Joao Pires on 04/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PTStations/PTStationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTStationsApp.swift
+//  PTStations
+//
+//  Created by Joao Pires on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PTStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
